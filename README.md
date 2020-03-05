980a8b5e9318651430591d0bb6168e3db9d7eb6f
Enrico Nardo <enr.nardo@gmail.com>
2020-03-05 15:33:34 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This package is based on https://github.com/dvanderburg/lumen-batched-request.
---
+++ src/BatchedRequestServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Codificio\BatchedRequest;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+
+use Codificio\BatchedRequest\BatchedRequest;
+
+class BatchedRequestServiceProvider extends ServiceProvider {
+
+    /**
+     * Attaches the route "batch" to handle batched requests
+     * Any HTTP POST requests to /batch will execute as a batched request
+     * @return null
+     */
+    public function boot() {
+
+        Route::post('batch', function(Request $request) {
+
+            $batch = $request->get('batch', []);
+
+            $batchedRequest = new BatchedRequest($batch, $request);
+
+            $batchedRequest->execute();
+
+            return $batchedRequest->getResponses();
+
+        });
+
+    }
+
+}
